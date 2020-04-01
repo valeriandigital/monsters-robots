e442ea46062a432f5f90e570c55677396e3ead5e
valeriandigital <valeriandigital@gmail.com>
2020-04-01 12:48:35 +0200

    Updates

---
+++ precache-manifest.fe8b7326d500cf0dc947b432944f7686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d2714ccc158e864a391719ef347d00",
+    "url": "/monsters-robots/index.html"
+  },
+  {
+    "revision": "3ce0dda3f3d00d944bb0",
+    "url": "/monsters-robots/static/css/main.19c83c0b.chunk.css"
+  },
+  {
+    "revision": "f6fa610514dce932c02b",
+    "url": "/monsters-robots/static/js/2.dc4cd316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-robots/static/js/2.dc4cd316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce0dda3f3d00d944bb0",
+    "url": "/monsters-robots/static/js/main.55711632.chunk.js"
+  },
+  {
+    "revision": "0f68bd02ed09b8a2f7a1",
+    "url": "/monsters-robots/static/js/runtime-main.35690ee1.js"
+  }
+]);
